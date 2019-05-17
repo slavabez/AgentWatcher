df4b467965d932a160f8cd6e4db40b3634f1b18c
Slava Bezgachev <bezgachev@gmail.com>
2019-05-17 11:15:32 +0600

    latest

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
 import * as fs from "fs";
+import chokidar from "chokidar";
 
-const watchDir = `/home/slava/ftp`;
+let watchDir = `/home/slava/ftp`;
 
+// If /home/slava/ftp doesn't exist, use the current dir
+try {
+  fs.accessSync(`/home/slava/ftp`, fs.constants.F_OK);
+} catch (e) {
+  watchDir = `./`;
+}
+
+console.log(`Application started! Watching directory ${watchDir}`);
+
+const watcher = chokidar.watch(watchDir);
+
+const log = console.log.bind(console);
+
+watcher.on("add", path => log(`File ${path} added`));
+watcher.on("change", path => log(`File ${path} changed`));
+watcher.on("unlink", path => log(`File ${path} deleted`));

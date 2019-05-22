f159497ca99ef0010dc1f1c6a290baf1518d68d4
Slava Bezgachev <bezgachev@gmail.com>
2019-05-22 14:23:40 +0600

    Change of plan, no more socket-io, simple express app

--- client/src/useIo.js
+++ client/src/useIo.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 const useSocket = () => {
   const [socket, setSocket] = useState(
-    io.connect(`http://localhost:5000`, {
+    io.connect(`http://192.168.10.8:5000`, {
       autoConnect: false,
       transports: ["websocket"]
     })
